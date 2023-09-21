0b204012c6d6c44a2feb5bdc022147a9d8187110
Alvin Gille <alvin.gille@elev.ga.ntig.se>
2023-09-21 10:57:19 +0200

    commitin

--- Slagsmål/Program.cs
+++ Slagsmål/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random generator = new Random();
+
+int slumptal = generator.Next(10, 20);
+
+Console.WriteLine(slumptal);
+
+
+int tal = 100;
+
+while (tal > 0)
+{
+    Console.WriteLine(tal);
+}
+
+Console.ReadLine();
