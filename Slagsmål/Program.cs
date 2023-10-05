61af21e0659cc05f54e87c9dd17fff1b10e8cd3e
Alvin Gille <alvin.gille@elev.ga.ntig.se>
2023-10-05 10:29:28 +0200

    87uybhuby

--- Slagsmål/Program.cs
+++ Slagsmål/Program.cs
@@ -1,15 +1,43 @@
-﻿Random generator = new Random();
+﻿using System.Runtime.InteropServices;
 
-int slumptal = generator.Next(10, 20);
+int Kennyhp = 100;
+int Kentahp = 100;
+Random generator = new Random();
 
-Console.WriteLine(slumptal);
+Console.WriteLine("Welcome to THE fighter game, press button to start");
+Console.ReadLine();
 
-
-int tal = 100;
-
-while (tal > 0)
+while (Kennyhp > 0 && Kentahp > 0)
 {
-    Console.WriteLine(tal);
+    Console.WriteLine("NEW ROUND");
+    Console.WriteLine($"Kenny: {Kennyhp} Kenta: {Kentahp}");
+
+    int Kennydamage = generator.Next(1, 21);
+    Kentahp -= Kennydamage;
+    Console.WriteLine($"Kenny does {Kennydamage} damage to Kenta");
+
+    int Kentadamage = generator.Next(1, 21);
+    Kennyhp -= Kentadamage;
+    Console.WriteLine($"Kenta does {Kentadamage} damage to Kenny");
+
+    Console.WriteLine($"Kenny: {Kennyhp} Kenta: {Kentahp}");
+
+    Console.WriteLine("Press button to continue");
+    Console.ReadLine();
+}
+
+Console.WriteLine("End of battle");
+if (Kennyhp <= 0 && Kentahp <= 0)
+{
+    Console.WriteLine("Draw");
+}
+else if (Kennyhp == 0)
+{
+    Console.WriteLine("Kenta WON!");
+}
+else
+{
+    Console.WriteLine("Kenny WON!");
 }
 
 Console.ReadLine();
